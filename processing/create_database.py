5ddacd31c06a5287da02e0f7897d78b1afb5ccf2
Jack <jtam64@my.bcit.ca>
2024-03-07 14:33:10 -0800

    try except

--- processing/create_database.py
+++ processing/create_database.py
@@ -2,16 +2,21 @@ import sqlite3
 
 conn = sqlite3.connect('stats.sqlite')
 
-c = conn.cursor()
-c.execute('''
-          CREATE TABLE IF NOT EXISTS stats
-          (id INTEGER PRIMARY KEY ASC, 
-           num_print_success INTEGER NOT NULL,
-           mm_used INTEGER NOT NULL,
-           num_failed_print INTEGER NOT NULL,
-           total_mm_wasted INTEGER NOT NULL,
-           last_updated VARCHAR(100) NOT NULL)
-          ''')
+try:
+    c = conn.cursor()
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS stats
+            (id INTEGER PRIMARY KEY ASC, 
+            num_print_success INTEGER NOT NULL,
+            mm_used INTEGER NOT NULL,
+            num_failed_print INTEGER NOT NULL,
+            total_mm_wasted INTEGER NOT NULL,
+            last_updated VARCHAR(100) NOT NULL)
+            ''')
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
+
+except:
+    print("already exists")
+    conn.close()
--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -9,32 +9,36 @@ db_conn = mysql.connector.connect(host=app_config["datastore"]["hostname"], user
 
 db_cursor = db_conn.cursor()
 
-db_cursor.execute('''
-    CREATE TABLE IF NOT EXISTS print_success
-    (
-        id INT NOT NULL AUTO_INCREMENT,
-        spool_id VARCHAR(250) NOT NULL,
-        printer_id VARCHAR(250) NOT NULL,
-        mm_used INTEGER NOT NULL,
-        colour VARCHAR(10) NOT NULL,
-        trace_id VARCHAR(250) NOT NULL,
-        date_created VARCHAR(100) NOT NULL,
-        CONSTRAINT print_success_pk PRIMARY KEY (id)
-    )
-    ''')
+try:
+    db_cursor.execute('''
+        CREATE TABLE IF NOT EXISTS print_success
+        (
+            id INT NOT NULL AUTO_INCREMENT,
+            spool_id VARCHAR(250) NOT NULL,
+            printer_id VARCHAR(250) NOT NULL,
+            mm_used INTEGER NOT NULL,
+            colour VARCHAR(10) NOT NULL,
+            trace_id VARCHAR(250) NOT NULL,
+            date_created VARCHAR(100) NOT NULL,
+            CONSTRAINT print_success_pk PRIMARY KEY (id)
+        )
+        ''')
 
-db_cursor.execute('''
-    CREATE TABLE IF NOT EXISTS failed_print
-    (
-        id INT NOT NULL AUTO_INCREMENT,
-        spool_id VARCHAR(250) NOT NULL,
-        printer_id VARCHAR(250) NOT NULL,
-        mm_wasted INTEGER NOT NULL,
-        timestamp VARCHAR(100) NOT NULL,
-        trace_id VARCHAR(250) NOT NULL,
-        date_created VARCHAR(100) NOT NULL,
-        CONSTRAINT failed_print_pk PRIMARY KEY (id)
-    )
-    ''')
-db_conn.commit()
-db_conn.close()
+    db_cursor.execute('''
+        CREATE TABLE IF NOT EXISTS failed_print
+        (
+            id INT NOT NULL AUTO_INCREMENT,
+            spool_id VARCHAR(250) NOT NULL,
+            printer_id VARCHAR(250) NOT NULL,
+            mm_wasted INTEGER NOT NULL,
+            timestamp VARCHAR(100) NOT NULL,
+            trace_id VARCHAR(250) NOT NULL,
+            date_created VARCHAR(100) NOT NULL,
+            CONSTRAINT failed_print_pk PRIMARY KEY (id)
+        )
+        ''')
+    db_conn.commit()
+    db_conn.close()
+except:
+    print("tables exist")
+    db_conn.close()
