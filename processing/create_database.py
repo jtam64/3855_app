f5c2f94fa31efeb093cd9bc93695f1c54a0019b3
Jack <jtam64@my.bcit.ca>
2024-03-25 13:35:18 -0700

    fix create db

--- processing/create_database.py
+++ processing/create_database.py
@@ -1,7 +1,20 @@
 import sqlite3
+import os
+import yaml
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
 
 def main():
-        conn = sqlite3.connect('stats.sqlite')
+        conn = sqlite3.connect(app_config["datastore"]["filename"])
 
         c = conn.cursor()
         c.execute('''
